cd39040924a68df19dfed7586440db4bfae18243
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2021-08-17 12:36:15 +0200

    :alien: changing export default structure

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+const greeting = (req, res) => {
+  res.status(200).json({ name: "Mickey Facts" });
+};
+
+export default greeting;
