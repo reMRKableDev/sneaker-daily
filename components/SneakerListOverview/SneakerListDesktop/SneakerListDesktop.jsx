493a4abb69a12bfea2b21840a084051db7eded44
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2021-08-19 18:01:17 +0200

    :sparkles: toggle sneaker view functionality done for mobile and desktop

--- components/SneakerListOverview/SneakerListDesktop/SneakerListDesktop.jsx
+++ components/SneakerListOverview/SneakerListDesktop/SneakerListDesktop.jsx
@@ -4,15 +4,82 @@ import SneakerCard from "../../SneakerCard";
 import styles from "./SneakerListDesktop.module.scss";
 
 const SneakerListDesktop = ({ sneakersList }) => {
-  const [sneakerView, setSneakerView] = useState();
+  const [isDoubleView, setIsDoubleView] = useState(false);
+  const [isQuadrupleView, setIsQuadrupleView] = useState(false);
 
-  useEffect(() => setSneakerView(4), []);
+  const handleToggleDoubleView = () => {
+    setIsQuadrupleView(false);
+    setIsDoubleView(true);
+  };
+
+  const handleToggleQuadrupleView = () => {
+    setIsDoubleView(false);
+    setIsQuadrupleView(true);
+  };
+
+  useEffect(() => setIsQuadrupleView(true), []);
 
   return (
-    <div className={cn(styles.sneakerListDesktop, styles.quadrupleView)}>
-      {sneakersList.map(({ sys, fields }) => (
-        <SneakerCard key={sys?.id} {...fields} />
-      ))}
+    <div className={cn(styles.sneakerListDesktopContainer)}>
+      <div className={styles.sneakerDisplayViewsContainer}>
+        <div
+          className={styles.sneakerDisplayDoubleView}
+          role="button"
+          tabIndex={0}
+          onClick={() => handleToggleDoubleView()}
+          onKeyPress={() => handleToggleDoubleView()}
+        >
+          <div
+            className={cn(styles.box, {
+              [styles.selectedSneakerView]: isDoubleView,
+            })}
+          ></div>
+          <div
+            className={cn(styles.box, {
+              [styles.selectedSneakerView]: isDoubleView,
+            })}
+          ></div>
+        </div>
+        <div
+          className={styles.sneakerDisplayQuadrupleView}
+          role="button"
+          tabIndex={0}
+          onClick={() => handleToggleQuadrupleView()}
+          onKeyPress={() => handleToggleQuadrupleView()}
+        >
+          <div
+            className={cn(styles.box, {
+              [styles.selectedSneakerView]: isQuadrupleView,
+            })}
+          ></div>
+          <div
+            className={cn(styles.box, {
+              [styles.selectedSneakerView]: isQuadrupleView,
+            })}
+          ></div>
+          <div
+            className={cn(styles.box, {
+              [styles.selectedSneakerView]: isQuadrupleView,
+            })}
+          ></div>
+          <div
+            className={cn(styles.box, {
+              [styles.selectedSneakerView]: isQuadrupleView,
+            })}
+          ></div>
+        </div>
+      </div>
+
+      <div
+        className={cn({
+          [styles.doubleView]: isDoubleView,
+          [styles.quadrupleView]: isQuadrupleView,
+        })}
+      >
+        {sneakersList.map(({ sys, fields }) => (
+          <SneakerCard key={sys?.id} {...fields} />
+        ))}
+      </div>
     </div>
   );
 };
