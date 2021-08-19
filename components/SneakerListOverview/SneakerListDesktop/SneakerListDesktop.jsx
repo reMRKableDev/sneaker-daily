8a06037580ab0567f880dc9db6fc137c933f8e20
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2021-08-19 15:18:08 +0200

    :lipstick: grid setup for desktop

--- components/SneakerListOverview/SneakerListDesktop/SneakerListDesktop.jsx
+++ components/SneakerListOverview/SneakerListDesktop/SneakerListDesktop.jsx
@@ -1,10 +1,15 @@
+import { useEffect, useState } from "react";
+import cn from "classnames";
 import SneakerCard from "../../SneakerCard";
 import styles from "./SneakerListDesktop.module.scss";
 
 const SneakerListDesktop = ({ sneakersList }) => {
-  const { sneakerListDesktop, quadrupleView } = styles;
+  const [sneakerView, setSneakerView] = useState();
+
+  useEffect(() => setSneakerView(4), []);
+
   return (
-    <div className={(sneakerListDesktop, quadrupleView)}>
+    <div className={cn(styles.sneakerListDesktop, styles.quadrupleView)}>
       {sneakersList.map(({ sys, fields }) => (
         <SneakerCard key={sys?.id} {...fields} />
       ))}
