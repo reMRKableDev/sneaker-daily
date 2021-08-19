b7a5c48915ba737845904ab5ca266f6b69f21801
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2021-08-19 17:00:49 +0200

    :sparkles: grid view changes

--- components/SneakerListOverview/SneakerListMobile/SneakerListMobile.jsx
+++ components/SneakerListOverview/SneakerListMobile/SneakerListMobile.jsx
@@ -4,14 +4,55 @@ import SneakerCard from "../../SneakerCard";
 import styles from "./SneakerListMobile.module.scss";
 
 const SneakerListMobile = ({ sneakersList }) => {
-  const [sneakerView, setSneakerView] = useState();
+  const [isSingleView, setIsSingleView] = useState(false);
+  const [isDoubleView, setIsDoubleView] = useState(false);
 
-  useEffect(() => setSneakerView(2), []);
+  const handleToggleSingleView = () => {
+    setIsDoubleView(false);
+    setIsSingleView(true);
+  };
+
+  const handleToggleDoubleView = () => {
+    setIsSingleView(false);
+    setIsDoubleView(true);
+  };
+
+  useEffect(() => setIsSingleView(true), []);
+
   return (
-    <div className={cn(styles.sneakerListMobile, styles.singleView)}>
-      {sneakersList.map(({ sys, fields }) => (
-        <SneakerCard key={sys?.id} {...fields} />
-      ))}
+    <div className={styles.sneakerListMobileContainer}>
+      <div className={styles.sneakerDisplayViewsContainer}>
+        <div
+          className={styles.sneakerDisplaySingleView}
+          role="button"
+          tabIndex={0}
+          onClick={() => handleToggleSingleView()}
+          onKeyPress={() => handleToggleSingleView()}
+        >
+          <div className={styles.box}></div>
+        </div>
+        <div
+          className={styles.sneakerDisplayDoubleView}
+          role="button"
+          tabIndex={0}
+          onClick={() => handleToggleDoubleView()}
+          onKeyPress={() => handleToggleDoubleView()}
+        >
+          <div className={styles.box}></div>
+          <div className={styles.box}></div>
+        </div>
+      </div>
+
+      <div
+        className={cn({
+          [styles.sneakerListSingleView]: isSingleView,
+          [styles.sneakerListDoubleView]: isDoubleView,
+        })}
+      >
+        {sneakersList.map(({ sys, fields }) => (
+          <SneakerCard key={sys?.id} {...fields} />
+        ))}
+      </div>
     </div>
   );
 };
