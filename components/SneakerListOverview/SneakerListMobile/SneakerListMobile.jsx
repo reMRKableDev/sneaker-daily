d71ff7820dcaa2a31b763228f890487ecc5095e6
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2021-08-19 15:38:14 +0200

    :sparkles: Classnames added to mobile sneaker view

--- components/SneakerListOverview/SneakerListMobile/SneakerListMobile.jsx
+++ components/SneakerListOverview/SneakerListMobile/SneakerListMobile.jsx
@@ -1,9 +1,14 @@
+import { useEffect, useState } from "react";
+import cn from "classnames";
 import SneakerCard from "../../SneakerCard";
 import styles from "./SneakerListMobile.module.scss";
 
 const SneakerListMobile = ({ sneakersList }) => {
+  const [sneakerView, setSneakerView] = useState();
+
+  useEffect(() => setSneakerView(2), []);
   return (
-    <div className={styles.sneakerListMobile}>
+    <div className={cn(styles.sneakerListMobile, styles.singleView)}>
       {sneakersList.map(({ sys, fields }) => (
         <SneakerCard key={sys?.id} {...fields} />
       ))}
