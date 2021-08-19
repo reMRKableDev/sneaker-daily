5c5af5d79bb2111c302460526a82c1fe9ee91632
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2021-08-19 15:05:12 +0200

    :sparkles: New visuals for mobile view

--- components/SneakerCard/SneakerCard.jsx
+++ components/SneakerCard/SneakerCard.jsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 import styles from "./SneakerCard.module.scss";
 
 const SneakerCard = ({ title, thumbnail, slug }) => {
-  const { card, content, info, actions } = styles;
-
+  const { cardContainer, cardTitle, cardTitleContainer, cardCallToAction } =
+    styles;
   const { url, details } = thumbnail?.fields?.file;
   const { width, height } = details?.image;
 
   return (
-    <article className={`flex-column ${card}`}>
-      <figure className="featured">
+    <article className={cardContainer}>
+      <figure>
         <Image
           src={`https:${url}`}
           width={width}
@@ -18,12 +18,12 @@ const SneakerCard = ({ title, thumbnail, slug }) => {
           alt="sneaker"
         />
       </figure>
-      <div className={content}>
-        <article className={info}>
-          <h4>{title}</h4>
-        </article>
+
+      <div className={cardTitleContainer}>
+        <h4 className={cardTitle}>{title}</h4>
       </div>
-      <div className={actions}>
+
+      <div className={cardCallToAction}>
         <Link href={`/sneakers/${slug}`}>
           <a>More Details</a>
         </Link>
