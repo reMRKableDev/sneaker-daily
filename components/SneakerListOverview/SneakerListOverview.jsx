4f10679edb5ecaf4896bc7fa2d31645467db1818
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2021-08-23 18:04:39 +0200

    :sparkles: Detecting device width alongside device type

--- components/SneakerListOverview/SneakerListOverview.jsx
+++ components/SneakerListOverview/SneakerListOverview.jsx
@@ -1,11 +1,13 @@
+import { useWindowWidth } from "@react-hook/window-size/throttled";
 import useIsDetectDevice from "../../hooks/useIsDetectDevice";
 import SneakerListDesktop from "./SneakerListDesktop";
 import SneakerListMobile from "./SneakerListMobile";
 
 const SneakerListOverview = ({ sneakersList }) => {
   const isDetectDevice = useIsDetectDevice();
+  const onlyDeviceWidth = useWindowWidth();
 
-  return isDetectDevice.isMobile() ? (
+  return isDetectDevice.isMobile() && onlyDeviceWidth < 768 ? (
     <SneakerListMobile sneakersList={sneakersList} />
   ) : (
     <SneakerListDesktop sneakersList={sneakersList} />
